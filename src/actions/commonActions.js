6b05cdcd0f16edeae7d72e78d7a65bf2171ed0ff
Lisa Wagner <wagnerl@mskcc.org>
2019-04-26 14:04:07 -0400

    revert last commit, wrong branch

--- src/actions/commonActions.js
+++ src/actions/commonActions.js
@@ -16,35 +16,35 @@ export const RECEIVE_SERVER_ERROR = 'RECEIVE_SERVER_ERROR'
 
 export function checkVersion(version) {
   return dispatch => {
-    dispatch({ type: REQUEST_CHECK_VERSION })
-    return axios
-      .get(API_ROOT + '/checkVersion?', {
-        params: {
-          version: version,
-        },
-      })
-      .then(response => {
-        dispatch({
-          type: RECEIVE_CHECK_VERSION_SUCCESS,
-          data: response.data,
-        })
-        return response
-      })
+    // dispatch({ type: REQUEST_CHECK_VERSION })
+    // return axios
+    //   .get(API_ROOT + '/checkVersion?', {
+    //     params: {
+    //       version: version,
+    //     },
+    //   })
+    //   .then(response => {
+    //     dispatch({
+    //       type: RECEIVE_CHECK_VERSION_SUCCESS,
+    //       data: response.data,
+    //     })
+    //     return response
+    //   })
 
-      .catch(error => {
-        if (error.response) {
-          dispatch({
-            type: RECEIVE_CHECK_VERSION_FAIL,
-            error: error,
-            errorMessage: error.response.data.message,
-          })
-        } else {
-          dispatch({
-            type: SERVER_ERROR,
-            error: 'error',
-          })
-        }
-      })
+    //   .catch(error => {
+    //     if (error.response) {
+    //       dispatch({
+    //         type: RECEIVE_CHECK_VERSION_FAIL,
+    //         error: error,
+    //         errorMessage: error.response.data.message,
+    //       })
+    //     } else {
+    dispatch({
+      type: SERVER_ERROR,
+      error: 'error',
+    })
+    //   }
+    // })
   }
 }
 
